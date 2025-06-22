30d9b8b034ebdb1e5915fd624921f15142c01107
Prometheus <toshiyasato219@gmail.com>
2025-06-22 14:32:14 +0900

    completed article list (end poits: everything)

--- src/components/news_display.jsx
+++ src/components/news_display.jsx
@@ -1,26 +1,58 @@
 import { useContext } from "react";
 import { NewsContext } from "./news_context";
+import image from "../assets/no_image.jpg"
 
 export const DisplayNew = () => {
     const {articles} = useContext(NewsContext);
+    const altImage = image
+
+    // Styles for article box 
+    const articleboxStyle = "flex space-x-4 p-4 rounded-lg shadow-md" + 
+    " bg-white border border-gray-200" +
+    " hover:bg-gray-200 transition duration-200 ease-in-out" + // selection animation on hover 
+    " dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" // darkmode settings 
+    // Styles for article lists info 
+    const infoStyle = "text-sm text-gray-600 dark:text-gray-400"
+
     // render articles card
     if (!articles || !Array.isArray(articles) || articles.length === 0)
-    {
-        return <p className="text-gray-100"> No news / articles to display </p>
+    {   // If there is no articles recieved then send fall back message 
+        return <p className="text-gray-500 
+        text-center py-10
+        dark:text-gray-400 "> No news / articles to display </p>
     }
     return (
-        <>
-            {articles.map((articles, index) =>(
-                <div key={articles.url} className="flex space-x-4 border p-4 rounded shadow w-[calc(100%-100px)]">
-                    <img className="w-40 h-24 object-cover rounded " 
-                    src={articles.urlToImage} 
-                    alt={`${articles.title}.png`}></img>
-                    <div>
-                        <p>{articles.title}</p>
-                        <p>source: {articles.source.name}</p>                  
+        <div className="flex flex-col space-y-4 w-[calc(100%-100px)]">
+            {articles.map((article, index) =>(
+                <a key={article.url} 
+                    className={`${articleboxStyle}`}
+                    href={article.url} // for now, TODO: take link to display the content of the article 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    > 
+
+                    <img className="w-[150px] h-[100px] object-cover rounded " 
+                    onError={(e)=>{ 
+                        e.currentTarget.onerror = null;
+                        e.currentTarget.src = altImage}}
+                    src={article.urlToImage || altImage} 
+                    />
+                    <div className="flex flex-col justify-between">
+                        <p className="text-lg font-semibold text-gray-900
+                            dark:text-white mb-1">{article.title}</p>
+
+                        <div className="flex flex-col space-y-2">            
+                            <p className={infoStyle}>
+                                {article.author && `Author: ${article.author}` /* Load only if exists */}  
+                                {article.publishedAt && ` - ${new Date(article.publishedAt).toLocaleDateString()}`}
+                            </p>
+                            <p className={infoStyle}>
+                                source: {article.source.name}
+                            </p>      
+                        </div>
                     </div>
-                </div>
+                </a>
             ))}
-        </>
+        </div>
     )
 }
